077615ebef32d1d0d68043b28cb5955944df5ea0
Francis <francis.menkes@gmail.com>
2016-11-29 21:26:38 -0300

    REFACTOR - CardSpec

--- spec/CardSpec.js
+++ spec/CardSpec.js
@@ -1,20 +1,21 @@
-
-  describe("Card", () => {
+describe("Card", () => {
   var Card = require("../app/Card");
+  var card;
+  var suit;
+  var value;
+
+  beforeEach(() => {
+    suit = "Hearts";
+    value = 2;
+
+    card = new Card(suit, value);
+  });
 
   it("should return the correct suit", () => {
-    var suit = "Hearts";
-    var value = 2;
-    var card = new Card(suit, value);
-
     expect(card.getSuit()).toEqual("Hearts");
   });
 
   it("should return the correct value", () => {
-    var suit = "Hearts";
-    var value = 2;
-    var card = new Card(suit, value);
-
     expect(card.getValue()).toEqual(2);
   });
 });
